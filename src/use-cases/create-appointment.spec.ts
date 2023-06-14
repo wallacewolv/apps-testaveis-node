cdf69a035ad323e8e482959f248e943d3e6449ae
wallace <wallace_wolv@hotmail.com>
2023-06-14 13:09:58 -0300

    test: startsAt deve ser diferente de agora

--- src/use-cases/create-appointment.spec.ts
+++ src/use-cases/create-appointment.spec.ts
@@ -5,11 +5,12 @@ import { Appointment } from "../entities/appointment";
 describe("Create Appointment", () => {
   it("should be able to create an appointment", () => {
     const createAppointment = new CreateAppointment();
-    
+
     const startsAt = new Date();
     const endsAt = new Date();
 
-    endsAt.setDate(endsAt.getDate() + 1);
+    startsAt.setDate(startsAt.getDate() + 1);
+    endsAt.setDate(endsAt.getDate() + 2);
 
     expect(
       createAppointment.execute({
