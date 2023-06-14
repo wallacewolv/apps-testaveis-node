a5e1826635a5245935e52fc5eba7e1d2c47d786a
wallace <wallace_wolv@hotmail.com>
2023-06-14 14:10:21 -0300

    feat: Incluindo validação no CreateAppointment para agendamentos repetidos, usando o AppointmentsRepository

--- src/use-cases/create-appointment.spec.ts
+++ src/use-cases/create-appointment.spec.ts
@@ -2,10 +2,12 @@ import { describe, expect, it } from "vitest";
 import { CreateAppointment } from "./create-appointment";
 import { Appointment } from "../entities/appointment";
 import { getFutureDate } from "../tests/utils/get-future-date";
+import { InMemoryAppointmentsRepository } from "../repositories/in-memory/in-memory-appointments-repository";
 
 describe("Create Appointment", () => {
   it("should be able to create an appointment", () => {
-    const createAppointment = new CreateAppointment();
+    const appointmentsRepository = new InMemoryAppointmentsRepository();
+    const createAppointment = new CreateAppointment(appointmentsRepository);
 
     const startsAt = getFutureDate("2023-06-14");
     const endsAt = getFutureDate("2023-06-15");
