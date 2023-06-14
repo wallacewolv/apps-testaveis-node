9b9d6eebe6711d595c1a1e62abd44055699b15db
wallace <wallace_wolv@hotmail.com>
2023-06-14 14:00:22 -0300

    feat: Função para validar se existe agendamentos iguais

--- src/repositories/appointments-repository.ts
+++ src/repositories/appointments-repository.ts
@@ -2,4 +2,8 @@ import { Appointment } from "../entities/appointment";
 
 export interface AppointmentsRepository {
   create(appointment: Appointment): Promise<void>;
+  findOverLappingAppointment(
+    startsAt: Date,
+    endsAt: Date
+  ): Promise<Appointment | null>;
 }
--- src/repositories/in-memory/in-memory-appointments-repository.ts
+++ src/repositories/in-memory/in-memory-appointments-repository.ts
@@ -1,3 +1,4 @@
+import { areIntervalsOverlapping } from "date-fns";
 import { Appointment } from "../../entities/appointment";
 import { AppointmentsRepository } from "../appointments-repository";
 
@@ -6,5 +7,24 @@ export class InMemoryAppointmentsRepository implements AppointmentsRepository {
 
   async create(appointment: Appointment): Promise<void> {
     this.items.push(appointment);
+  }
+
+  async findOverLappingAppointment(
+    startsAt: Date,
+    endsAt: Date
+  ): Promise<Appointment | null> {
+    const overLappingAppointment = this.items.find((appointment) => {
+      return areIntervalsOverlapping(
+        { start: startsAt, end: endsAt },
+        { start: appointment.startsAt, end: appointment.endsAt },
+        { inclusive: true }
+      );
+    });
+
+    if (!overLappingAppointment) {
+      return null;
+    }
+
+    return overLappingAppointment;
   }
 }
